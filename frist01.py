d30501e4a400f9e602f94955a90380ce2d5a5f94
nongnoonjuju <164063@wsk.ac.th>
2026-06-16 04:08:47 +0000

    frist01.py

---
+++ frist01.py
@@ -0,0 +1,6 @@
+print("โปรแกรมคำนวนพื้นที่ของสามเหลี่ยม")
+h=float(input("Enter hight:"))
+b=float(input("Enter base:"))
+area=0.5*h*b
+print("พื้นที่ของสามเหลี่ยมเท่ากับ",area)
+print("จัดทำโดย นางสาวจุฑามณี บาลี เลขที่16")
